92c56447f2eddd2e4b515b5bf6f968ae3072bc17
Jelani Joseph <jelani.joseph1@marist.edu>
2019-10-22 15:38:30 -0400

    Ready for part 2

--- project.py
+++ project.py
@@ -9,10 +9,11 @@ def project():
     fl2 = str("second floor")
     fl3 = str("third floor")
     fl4 = str("Fourth floor")
-    fl5 = str("Fith floor")
+    fl5 = str("Last floor")
     final = str("the ground")
-    start = title(fl0)
+    fl_visited = False
     score = 0
+    start = title(fl0)
     game_start = game_loop(fl1, score, fl2, fl3, fl4, fl5)
     email_jel = str("jelani.joseph1@marist.edu")
     Ending = end_screen(email_jel, score, final)
@@ -33,13 +34,16 @@ def title(fl0):
 
 # Function to add to score, and show location visited
 def change(score, location):
-    score = score + 5
-    print("Your score is now:", score)
+    score +=5
+    my_list = ['Bottom of a tower', 'First floor', 'Second floor',
+    'Fourth floor', 'Last floor', 'the ground']
+    print("Your score is now:", str(score))
     print("You have passed", location)
+    print("Going up to the", my_list[score // 5], "\n")
     return(score, location)
 
 
-# Main game loop 
+# Main game loop, when you see player take it out for the variable
 def game_loop(fl1, score, fl2, fl3, fl4, fl5):
     while True:
         print("You start on the " + fl1 + " and see ")
@@ -47,11 +51,12 @@ def game_loop(fl1, score, fl2, fl3, fl4, fl5):
         print("The golem rushes to attack you but you quickly dodge it, ")
         print("you aim your attack at the core, slaying the golem in one blow")
         print("you insert the core from the golem into the slothole ")
-        print("the doors slide open and reveal stairs to the next floor")
+        print("the doors slide open and reveal stairs to the next floor\n")
         location = fl1
         if location == fl1:
-            change(score, location)
-        print("Going up to " + fl2 + ".\n")
+            fl_visited = True
+            score, location = change(score, location)
+        print("Going up to the " + fl2 + ".\n")
         print("You wait for a while staring at the staircase, ")
         print("catching your breath after the sudden burst of addrenaline")
         print("After a while you calm yourself practicing breathing techniques ")
@@ -82,8 +87,8 @@ def game_loop(fl1, score, fl2, fl3, fl4, fl5):
         print(" This time it is a set of ladders.\n")
         location = fl2
         if location == fl2:
-            change(score, location)
-        print(" After climbing for some time you reach " + fl3 + ",\n")
+            fl_visited = True
+            score, location = change(score, location)
         print(" and notice how foggy and blocked the area seems")
         print(" After looking around you find the area ")
         print("looks almost maze-like... in fact it is a maze!")
@@ -97,11 +102,11 @@ def game_loop(fl1, score, fl2, fl3, fl4, fl5):
         print(" Choosing path on the right, you manage to escape the beast. ")
         print("You take a moment to calm down, and catch your breath.")
         print(" Soon you get back up and continue through the maze")
-        print("In your wondering you stumble upon the next ladder")
+        print("In your wondering you stumble upon the next ladder\n")
         location = fl3
         if location == fl3:
-            change(score, location)
-        print(" You climb up to the ", fl4 + ".")
+            fl_visited = True
+            score, location = change(score, location)
         print("you realize that the ground is strangely soft.")
         print(" Upon closer inspection you notice that the ground resembles")
         print(" a marsh enviroment, and that there are multiple puddles")
@@ -114,12 +119,11 @@ def game_loop(fl1, score, fl2, fl3, fl4, fl5):
         # add a input for the player to make a choice here
         print(" After some time you decide what to do")
         # add an outcome based on input here
-        print(" Barely making it out you reach the ladders.")
+        print(" Barely making it out you reach the ladders.\n")
         location = fl4
         if location == fl4:
-            change(score, location)
-            print("\n")
-        print("Upon reaching ",fl5, ", you take a moment to observe the enviroment")
+            fl_visited = True
+            score, location = change(score, location)
         print("Looking around you see the area is completely flat and there is only a mirror")
         print("Curious, you contemplate if you look into the mirror ")
         # add input for player to either look at mirror or walk away
@@ -139,20 +143,21 @@ def game_loop(fl1, score, fl2, fl3, fl4, fl5):
         print("You see the being rushing toward you, you know its trying to deliver the finishing blow.")
         print(" You cannot dodge, as you are in too much pain to move that fast.")
         # add input for player to: defend, or attack
-        print("The force of the being sends you flying through the wall, and you find yourself free falling")
+        print("The force of the being sends you flying through the wall, and you find yourself free falling\n")
         location = fl5
         if location == fl5:
-            change(score, location)
+            fl_visited = True
+            score, location = change(score, location)
         break
         
 # Ending screen with copyright
 def end_screen(email_jel, score, final):
-    input("Your current location is now in mid-air, as you were sent flying")
+    print("Your current location is now in mid-air, as you were sent flying")
     print("You have now reached " + final + " soon you feel a second ")
     print("burst of pain and then everything gets numb as your consciousness fades...\n")
     location = final
     if location == final:
-        change(score, location)
+        fl_visited = True
     print("You wake up realizing that it was all a dream, yet your body aches all over\n")
     print("Copyright: This game was created by Jelani, and Daniel\n")
     print("If you have any questions feel free to email " + email_jel)
