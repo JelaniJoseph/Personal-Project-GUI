abca25ba6f72d48f76cb65095902d1334abc9f0f
Jelani Joseph <jelani.joseph1@marist.edu>
2019-11-22 23:40:57 -0500

    Class Inheretance issues W.I.P

--- zone.py
+++ zone.py
@@ -1,9 +1,11 @@
+
 class Zone:
-    def __init__(self, name, description, actions, after, **kwargs):
+    def __init__(self, name, description, actions, after):
         self.name = name
         self.description = description
-        self.after = after
         self.actions = actions
+        self.after = after
+        self.actions_taken = 0
         
 
 
@@ -18,20 +20,44 @@ class Zone:
 class Forest(Zone):
 
     def __init__(self):
-        Zone.__init__(self, name = "Forest", description = "You enter the Ominous Woods, there seems to be two paths",
-        after = "as you walk through the path you hear a sinister roar, it seems your choices will now have consequences.",
-        actions = {"left": "You take the left-most path", "right": "You take the right-side path", "Search": "You found a backpack!"})
+        Zone.__init__(self, "Forest", "You enter the Ominous Woods, there seems to be two paths",
+        {"left": "You take the left-most path", "right": "You take the right-side path", "search": "You found a backpack!"},
+        "as you continue you hear a sinister roar, it seems your choices will now have consequences.")
 
 
-    def choose_action(self):
-        for key, value in self.actions.items():
-            print("[]", key)
-        action = input("\nAction >> ").lower()
-        if action in self.actions:
-            print(self.actions[action])
+    def choose_action(self, actions_taken, forest):
+        while True:
+            for key, value in self.actions.items():
+                print("[]", key)
+            action = input("\nAction >> ").lower()
+            actions_taken += 1
+            if action in self.actions:
+                print(self.actions[action])
+                print(Forest.after_desc())
+                break
+            else:      
+                action = input("\n Incorrect, Try Again >>").lower()
+            
 
+class Forest_Path(Zone):
+    def __init__(self):
+        Zone.__init__(self, "Forest", "As you continue through the Forest you see a glint in the trees, and two pathways.", 
+        {"right": "You take the right path","left": "You take the left path, but it seems to be a dead end!","search": "You found something"}, 
+         "You continue on!")
 
-
+    def choose_action2(self, actions_taken, Forest_Path):
+        while True:
+            actions_taken = 0
+            for key, value in self.actions.items():
+                print("[]", key)
+            action = input("\nAction >> ").lower()
+            actions_taken += 1
+            if action in self.actions:
+                print(self.actions[action])
+                print(Forest_Path.after_desc())
+                break
+            else:      
+                action = input("\nIncorrect, Try Again >>").lower()
 
 # class Cabin(Zone):
 #     def __init__(self):
@@ -67,6 +93,8 @@ def main():
     actions_taken = 0
     forest = Forest()
     forest.describe()
-    forest.choose_action()
-    forest.after_desc()
+    # forest.choose_action(actions_taken, forest)
+    forest2 = Forest_Path()
+    forest2.describe()
+    forest2.choose_action2(Forest_Path, actions_taken)
 main()
