52a215af33558bfe294e3078156f9fa210349436
Jelani Joseph <jelani.joseph1@marist.edu>
2019-11-23 11:20:09 -0500

    Re-written story in progress, 50%

--- zone.py
+++ zone.py
@@ -1,4 +1,3 @@
-
 class Zone:
     def __init__(self, name, description, actions, after):
         self.name = name
@@ -6,13 +5,22 @@ class Zone:
         self.actions = actions
         self.after = after
         self.actions_taken = 0
-        
-
 
     def describe(self):
         print(self.name)
         print("=========")
         print(self.description)
+
+    def choose_action(self):
+        while True:
+            for key, value in self.actions.items():
+                print("[]", key.capitalize())
+            action = input("\nAction >> ").lower()
+            if action in self.actions:
+                print(self.actions[action])
+                return(True)
+            else:      
+                action = input("\n Incorrect, Try Again >>").lower()
 
     def after_desc(self):
         print(self.after)
@@ -22,52 +30,38 @@ class Forest(Zone):
     def __init__(self):
         Zone.__init__(self, "Forest", "You enter the Ominous Woods, there seems to be two paths",
         {"left": "You take the left-most path", "right": "You take the right-side path", "search": "You found a backpack!"},
-        "as you continue you hear a sinister roar, it seems your choices will now have consequences.")
+        "as you continue you hear a sinister roar, it seems your choices will now have consequences.\n")
 
-
-    def choose_action(self, actions_taken, forest):
-        while True:
-            for key, value in self.actions.items():
-                print("[]", key)
-            action = input("\nAction >> ").lower()
-            actions_taken += 1
-            if action in self.actions:
-                print(self.actions[action])
-                print(Forest.after_desc())
-                break
-            else:      
-                action = input("\n Incorrect, Try Again >>").lower()
-            
 
 class Forest_Path(Zone):
     def __init__(self):
-        Zone.__init__(self, "Forest", "As you continue through the Forest you see a glint in the trees, and two pathways.", 
-        {"right": "You take the right path","left": "You take the left path, but it seems to be a dead end!","search": "You found something"}, 
-         "You continue on!")
+        Zone.__init__(self, "Forest Pathway", "As you continue through the Forest you see a glint in the trees, and two pathways.", 
+        {"right": "You take the right path","left": "You take the left path, but it seems to be a dead end!", "search": "You found something"}, 
+         "You continue on!\n")
 
-    def choose_action2(self, actions_taken, Forest_Path):
-        while True:
-            actions_taken = 0
-            for key, value in self.actions.items():
-                print("[]", key)
-            action = input("\nAction >> ").lower()
-            actions_taken += 1
-            if action in self.actions:
-                print(self.actions[action])
-                print(Forest_Path.after_desc())
-                break
-            else:      
-                action = input("\nIncorrect, Try Again >>").lower()
 
-# class Cabin(Zone):
-#     def __init__(self):
-#         Zone.__init__(self, name = "Cabin", description = "You squint and can barely make out the figure of a log cabin",
-#         actions = {"Search": "You found a coat!", "Rest": " You decide to rest a while...",
-#             "Find an exit": "You look for a way out of the cabin"})
+class Cabin(Zone):
+    def __init__(self):
+        Zone.__init__(self, "Cabin", "You squint and can barely make out the figure of a log cabin",
+        {"search": "You found a coat, and a map!", "rest": " You decide to rest a while...",
+        "find an exit": "You look for a way out of the cabin"}, "You prepare yourself, and push forward!\n")
 
-# class Tundra(Zone):
-#     def __init__(self):
-#         Zone.__init__(self, name = "Tundra" )
+
+class Tundra(Zone):
+    def __init__(self):
+        Zone.__init__(self, "Tundra", "The cold chips away at your very being",
+         {"walk": "You try to push through the blizzard", "inventory": "within the backpack there was a wisp in a bottle!",
+         "map": "You open the map and see the pathway you must follow","backward": "it's too cold, you go back in the cabin",},
+         "After some time you reach an icy lake\n" )
+
+
+class Tundra_Path(Zone):
+    def __init__(self):
+        Zone.__init__(self, "Tundra", "as you stare ath the ocean, you hear something dangerous approaching you",
+        {"jump": "Afraid for your life you jump into the ocean",
+         "fight": "you stand your ground!, but the monster claps you"}, "You sink into the icy depths of the water")
+
+
 # class Ocean(Zone):
 #     def __init__(self):
 #         Zone.__init__(self, name = "Ocean" )
@@ -88,13 +82,25 @@ class Forest_Path(Zone):
 #         Zone.__init__(self, name = "Hospital" )
     
 
-    
 def main():
-    actions_taken = 0
     forest = Forest()
     forest.describe()
-    # forest.choose_action(actions_taken, forest)
+    forest.choose_action()
+    forest.after_desc()
     forest2 = Forest_Path()
     forest2.describe()
-    forest2.choose_action2(Forest_Path, actions_taken)
+    forest2.choose_action()
+    forest2.after_desc()
+    cab = Cabin()
+    cab.describe()
+    cab.choose_action()
+    cab.after_desc()
+    ice = Tundra()
+    ice.describe()
+    ice.choose_action()
+    ice.after_desc()
+    frozen = Tundra_Path()
+    frozen.describe()
+    frozen.choose_action()
+    frozen.after_desc()
 main()
