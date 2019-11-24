c12e21a40bbae4506ddf740e2e8faa00b0e176e2
Jelani Joseph <jelani.joseph1@marist.edu>
2019-11-23 21:51:18 -0500

    Story finished, needs work on was searched and consequence

--- zone.py
+++ zone.py
@@ -1,50 +1,61 @@
 class Zone:
-    def __init__(self, name, description, actions, after):
+    def __init__(self, name, description, actions, after, consequence, visited):
         self.name = name
         self.description = description
         self.actions = actions
         self.after = after
-        self.actions_taken = 0
+        self.consequence = consequence
+        self.visited = False
+
 
     def describe(self):
         print(self.name)
         print("=========")
         print(self.description)
 
+
     def choose_action(self):
-        while True:
-            for key, value in self.actions.items():
-                print("[]", key.capitalize())
-            action = input("\nAction >> ").lower()
-            if action in self.actions:
-                print(self.actions[action])
-                return(True)
-            else:      
-                action = input("\n Incorrect, Try Again >>").lower()
+        for key, value in self.actions.items():
+            print("[]", key.capitalize())
+        action = input("\nAction >> ").lower()
+        if action in self.actions:
+            print(self.actions[action])
+            return(True)
+        else:      
+            action = input("\n Incorrect, Try Again >>").lower()
+
+
+    def area_visited(self):
+        if self.visited == True:
+            print("Playername has visited: ", self.name)
+
 
     def after_desc(self):
         print(self.after)
+
 
 class Forest(Zone):
 
     def __init__(self):
         Zone.__init__(self, "Forest", "You enter the Ominous Woods, there seems to be two paths",
         {"left": "You take the left-most path", "right": "You take the right-side path", "search": "You found a backpack!"},
-        "as you continue you hear a sinister roar, it seems your choices will now have consequences.\n")
+        "as you continue you hear a sinister roar, it seems your choices will now have consequences.\n",
+        "text is within here", True)
 
 
 class Forest_Path(Zone):
     def __init__(self):
         Zone.__init__(self, "Forest Pathway", "As you continue through the Forest you see a glint in the trees, and two pathways.", 
         {"right": "You take the right path","left": "You take the left path, but it seems to be a dead end!", "search": "You found something"}, 
-         "You continue on!\n")
+         "You continue on!\n", "A pitch black humanoid creature with a smile appears, and kills you", True )
 
 
 class Cabin(Zone):
     def __init__(self):
         Zone.__init__(self, "Cabin", "You squint and can barely make out the figure of a log cabin",
         {"search": "You found a coat, and a map!", "rest": " You decide to rest a while...",
-        "find an exit": "You look for a way out of the cabin"}, "You prepare yourself, and push forward!\n")
+        "find an exit": "You look for a way out of the cabin"}, "You prepare yourself, and push forward!\n",
+        "text", True)
 
 
 class Tundra(Zone):
@@ -52,37 +63,49 @@ class Tundra(Zone):
         Zone.__init__(self, "Tundra", "The cold chips away at your very being",
          {"walk": "You try to push through the blizzard", "inventory": "within the backpack there was a wisp in a bottle!",
          "map": "You open the map and see the pathway you must follow","backward": "it's too cold, you go back in the cabin",},
-         "After some time you reach an icy lake\n" )
+         "After some time you reach an icy lake\n", "text", True)
 
 
 class Tundra_Path(Zone):
     def __init__(self):
         Zone.__init__(self, "Tundra", "as you stare ath the ocean, you hear something dangerous approaching you",
         {"jump": "Afraid for your life you jump into the ocean",
-         "fight": "you stand your ground!, but the monster claps you"}, "You sink into the icy depths of the water")
+         "fight": "you stand your ground!, but the monster claps you"}, "You sink into the icy depths of the water",
+         "text",visited= True)
 
 
 class Ocean(Zone):
     def __init__(self):
         Zone.__init__(self, "Ocean", "you're underwater", {"swim": "you swim downwards and reach an underwater cave"},
-        "As you enter the cave, the entrance behind you closes and the water dissapears")
+        "As you enter the cave, the entrance behind you closes and the water dissapears", "text", True)
+
 
 class Cave(Zone):
     def __init__(self):
         Zone.__init__(self, "Cave", "the area somehow feels alive...", {"search": "you found a skull key hidden in the rocks!",
          "continue": "you walk down the cave and soon find yourself in a series of tunnels"}, 
-         "it's almost as if the area is shifting all on its own...")
+         "it's almost as if the area is shifting all on its own...", "text", True)
 
-# class Tunnel(Zone):
-#     def __init__(self):
-#         Zone.__init__(self, name = "Tunnel" )
 
-# class House(Zone):
-#     def __init__(self):
-#         Zone.__init__(self, name = "House" )
-# class Hospital(Zone):
-#     def __init__(self):
-#         Zone.__init__(self, name = "Hospital" )
+class Tunnel(Zone):
+    def __init__(self):
+        Zone.__init__(self, "Tunnel", "it's dark and you can sense something folowing you", 
+        {"map": "You pull out the map and see that you must make a right turn",
+        "left": "You take the left-most path, but its a dead end...", "right": "you take the right-most path"},
+        "Soon you see a bright light and as you go towards it your area begins to change.",
+        "You feel the tunnel begin to move and soon you are enveloped by the floor", True )
+
+
+class House(Zone):
+    def __init__(self):
+        Zone.__init__(self, "House", "Feels safe and cozy...", {"wander": "you begin to look in all of the rooms",
+        "rest": "you decide to sit down and rest for a while"}, "text", True )
+
+
+class Hospital(Zone):
+    def __init__(self):
+        Zone.__init__(self, "Hospital", "strangely feels more real than what just happened...",
+        {"waken": "You open your eyes and realize that you are now in a hospital"}, "text", True )
     
 
 def main():
