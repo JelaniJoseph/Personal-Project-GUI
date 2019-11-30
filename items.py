a8310d76c5846b9a7d0442235492e6a27a87fe42
Jelani.Joseph <jelani.joseph1@marist.edu>
2019-11-29 22:32:50 -0500

    Class file Completed!

--- items.py
+++ items.py
@@ -1,19 +1,22 @@
-from classes import Item, Location
+class Items:
 
-map_description = ""
+    def __init__(self, name, description, use):
+        self.name = name
+        self.description = description
+        self.use = use
 
-class Map(Item):
-    def __init__(self, current_location):
-        Item.__init__(self, map_description)
-        self.current_location = current_location
+class Wisp_in_bottle(Items):
+    def __init__(self):
+        Items.__init__(self, 'A Wisp in a Bottle', 'Will lead you to vicotry!', 1)
 
-    def update_loc(self, new_location):
-        if isinstance(new_location, Location):
-            self.current_location = new_location
+class Coat(Items):
+    def __init__(self):
+        Items.__init__(self, 'Coat', 'Looks nice and warm', 10)
 
-    def get_location(self):
-        return self.current_location
+class Backpack(Items):
+    def __init__(self):
+        Items.__init__(self, 'Red Backpack', 'Looks spacious', 10)
 
-
-
-        
+class Coin(Items):
+    def __init__(self):
+        Items.__init__(self, 'A coin with a skull on it', 'Just looking at it makes you feel uneasy', 1)
