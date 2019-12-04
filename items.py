af068869272decd88d0e271067d435e7931df7d9
Jelani.Joseph <jelani.joseph1@marist.edu>
2019-12-03 19:01:32 -0500

    Item class fully Operational!

--- items.py
+++ items.py
@@ -8,18 +8,11 @@ class Items:
     def get_name(self):
         return(self.name)
 
-class Wisp_in_bottle(Items):
-    def __init__(self):
-        Items.__init__(self, 'A Wisp in a Bottle', 'Will lead you to vicotry!', 1)
 
-class Coat(Items):
-    def __init__(self):
-        Items.__init__(self, 'Coat', 'Looks nice and warm', 10)
+Wisp_in_bottle = Items(name = 'A Wisp in a Bottle', description= 'Will lead you to vicotry!', use= 1)
 
-class Backpack(Items):
-    def __init__(self):
-        Items.__init__(self, 'Red Backpack', 'Looks spacious', 10)
+backpack = Items('Red Backpack', 'Looks spacious', 10)
 
-class Coin(Items):
-    def __init__(self):
-        Items.__init__(self, 'A coin with a skull on it', 'Just looking at it makes you feel uneasy', 1)
+coat = Items('Coat', 'Looks nice and warm', 10)
+
+coin = Items('A coin with a skull on it', 'Just looking at it makes you feel uneasy', 1)
