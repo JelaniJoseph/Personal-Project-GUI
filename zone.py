d7358bdab16f070b1db7ce7416aee104bd540bc2
Jelani.Joseph <jelani.joseph1@marist.edu>
2019-12-06 00:02:12 -0500

    perfectly functional but inventory use still needed

--- zone.py
+++ zone.py
@@ -1,5 +1,4 @@
 from items import *
-
 class Zone:
     def __init__(self, name, description, after, consequence, item_list, actions):
         self.name = name
@@ -10,6 +9,7 @@ class Zone:
         self.visited = False
         self.searched = False
         self.actions = actions
+        self.conditional_statement = True
 
 
     def describe(self):
@@ -45,6 +45,7 @@ class Zone:
                     self.choose_action(player)
         elif action == 'inventory':
             player.inventory_show()
+            player.inventory_use()
             player.set_act_taken()
             self.choose_action(player)
         elif action == 'location':
@@ -71,32 +72,35 @@ class Zone:
         return(self.item_list)
 
 
+    # checks to see if area was visited or not
     def area_visited(self):
         if self.visited == True:
             print("Playername has visited: ", self.name)
 
 
+    # checks to see if area was searched or not
     def was_searched(self):
         if self.searched == True:
             print("You have searched this locale")
 
 
+    # Prints the after description of the locale 
     def after_desc(self):
         print(self.after)
 
 
+    # Used to end the game if key is typed, prints value of key, consequence of locale, and then ends the game.
     def situational(self, action):
         print(self.actions[action])
         print(self.consequence)
         print("You Died...\n")
-        quit()
 
 
     def forest_path_actions(self, locations, player):
         for key, value in self.actions.items():
             print("[]", key.capitalize())
         action = input("\nAction >> ").lower()
-        if action in self.actions and action != 'location' and action != 'score' and action != 'inventory' and action !='left':
+        if action in self.actions and action != 'location' and action != 'score' and action != 'inventory' and action != 'left':
             print(self.actions[action])
             player.set_act_taken()
             self.visited = True
@@ -111,27 +115,33 @@ class Zone:
                 if decide == 'take':
                     player.take()
                     player.set_act_taken()
+                    return(True)
                 elif decide == 'drop':
                     print(player.drop_item())
                     player.set_act_taken()
+                    return(True)
                 else:
                     decide = ("Try again, take or drop?")
                     self.choose_action(player)
-            if action == 'inventory':
-                player.inventory_show()
-                player.set_act_taken()
-                self.choose_action(player)
+        elif action == 'inventory':
+            player.inventory_show()
+            player.inventory_use()
+            player.set_act_taken()
+            self.choose_action(player)
         elif action == 'location':
             print(self.actions[action] + self.name)
             player.set_act_taken()
             self.choose_action(player)
+            return(True)
         elif action == 'left':
             player.set_act_taken()
             locations[1].situational(action)
+            return(False)
         elif action == 'score':
             print(player.getname(), "score is: ", player.score_return())
             player.set_act_taken()
             self.choose_action(player)
+            return(True)
         else:
             print("\n Incorrect, Try Again >>")
             player.set_act_taken()
@@ -142,11 +152,15 @@ class Zone:
         for key, value in self.actions.items():
             print("[]", key.capitalize())
         action = input("\nAction >> ").lower()
-        if action in self.actions and action != 'location' and action != 'score' and action != 'inventory' and action !='fight':
+        if coat not in player.inventory:
+            print("Without a coat you get extreme frostbite, and begin to crack")
+            locations[4].situational(action)
+        elif action in self.actions and action != 'location' and action != 'score' and action != 'inventory' and action !='fight':
             print(self.actions[action])
             player.set_act_taken()
             self.visited = True
             print(self.after_desc())
+            return(True)
             if action  == 'search':
                 print(self.actions[action])
                 player.set_act_taken()
@@ -157,25 +171,34 @@ class Zone:
                 if decide == 'take':
                     player.take()
                     player.set_act_taken()
+                    return(True)
                 elif decide == 'drop':
                     print(player.drop_item())
                     player.set_act_taken()
+                    return(True)
                 else:
                     decide = ("Try again, take or drop?")
                     self.choose_action(player)
-            if action == 'inventory':
-                player.inventory_show()
+        elif action == 'inventory':
+            player.inventory_show()
+            player.inventory_use()
+            player.set_act_taken()
+            self.choose_action(player)
+            return(True)
         elif action == 'location':
             print(self.actions[action] + self.name)
             player.set_act_taken()
             self.choose_action(player)
+            return(True)
         elif action == 'score':
             print(player.getname(), "score is: ", player.score_return())
             player.set_act_taken()
             self.choose_action(player)
+            return(True)
         elif action == 'fight':
             player.set_act_taken()
             locations[4].situational(action)
+            return(False)
         else:
             action = input("\n Incorrect, Try Again >>").lower()
             player.set_act_taken()
@@ -191,6 +214,7 @@ class Zone:
             player.set_act_taken()
             self.visited = True
             print(self.after_desc())
+            return(True)
             if action  == 'search':
                 print(self.actions[action])
                 player.set_act_taken()
@@ -201,25 +225,34 @@ class Zone:
                 if decide == 'take':
                     player.take()
                     player.set_act_taken()
+                    return(True)
                 elif decide == 'drop':
                     print(player.drop_item())
                     player.set_act_taken()
+                    return(True)
                 else:
                     decide = ("Try again, take or drop?")
                     self.choose_action(player)
-            if action == 'inventory':
-                player.inventory_show()
+        elif action == 'inventory':
+            player.inventory_show()
+            player.inventory_use()
+            player.set_act_taken()
+            self.choose_action(player)
+            return(True)
         elif action == 'score':
             print(player.getname(), "score is: ", player.score_return())
             player.set_act_taken()
             self.choose_action(player)
+            return(True)
         elif action == 'location':
             print(self.actions[action] + self.name)
             player.set_act_taken()
             self.choose_action(player)
+            return(True)
         elif action == 'left':
             player.set_act_taken()
-            locations[7].situational(self, action)
+            locations[7].situational(action)
+            return(False)
         else:
             action = input("\n Incorrect, Try Again >>").lower()
             player.set_act_taken()
@@ -227,8 +260,8 @@ class Zone:
 
 
 
-
 def locale_data(backpack, Wisp_in_bottle, coat, coin):
+
     Forest = Zone(name="Forest",description= "You enter the Ominous Woods, there seems to be two paths",
         after="as you continue you hear a sinister roar, it seems your choices will now have consequences.\n",
         consequence= "text is within here", item_list= [backpack], 
@@ -237,14 +270,14 @@ def locale_data(backpack, Wisp_in_bottle, coat, coin):
         'location': "You are in: ", 'score': "Your score is: ", 'inventory': "your inventory opens!"})
 
     Forest_Path = Zone("Forest Pathway", 
-    "As you continue through the Forest you see a glint in the trees, and two pathways.", "You continue on!\n", 
+        "As you continue through the Forest you see a glint in the trees, and two pathways.", "You continue on!\n", 
         "A pitch black humanoid creature with a smile appears, and kills you", [coin], 
         {"right": "You take the right path", "left": "You take the left path, but it seems to be a dead end!",
         "search": "You found a strange coin", 'location': "You are in: ", 'score': "Your score is: ",
         'inventory': "your inventory opens!"})
 
     Cabin = Zone("Cabin", "You squint and can barely make out the figure of a log cabin", 
-    "You prepare yourself, and push forward!\n", "text", [Wisp_in_bottle, coat],
+        "You prepare yourself, and push forward!\n", "text", [Wisp_in_bottle, coat],
         {"search": "You found a coat, and a strange flask!",
         "rest": " You decide to rest a while...", "find an exit": "You look for a way out of the cabin",
         'location': "You are in: ", 'score': "Your score is: ",'inventory': "your inventory opens!"})
@@ -258,16 +291,16 @@ def locale_data(backpack, Wisp_in_bottle, coat, coin):
         'location': "You are in: ", 'score': "Your score is: ", 'inventory': "your inventory opens!"})
 
     Tundra_Path = Zone("Tundra", "as you stare ath the ocean, you hear something dangerous approaching you"
-    , "You sink into the icy depths of the water",
+        , "You sink into the icy depths of the water",
         "you lay there bleeding out, and cold.", [None], 
         {"jump": "Afraid for your life you jump into the ocean", 
         "fight": "you stand your ground!, but the monster claps you",
         'location': "You are in: ", 'score': "Your score is: ", 'inventory': "your inventory opens!"})
 
     Ocean = Zone("Ocean", "you're underwater",
-    "As you enter the cave, the entrance behind you closes and the water dissapears", "text", [None],
-    {"swim": "you swim downwards and reach an underwater cave",
-    'location': "You are in: ", 'score': "Your score is: ", 'inventory': "your inventory opens!"})
+        "As you enter the cave, the entrance behind you closes and the water dissapears", "text", [None],
+        {"swim": "you swim downwards and reach an underwater cave",
+        'location': "You are in: ", 'score': "Your score is: ", 'inventory': "your inventory opens!"})
 
     Cave = Zone("Cave", "the area somehow feels alive...", 
         "it's almost as if the area is shifting all on its own...", "text", [None],
@@ -276,11 +309,11 @@ def locale_data(backpack, Wisp_in_bottle, coat, coin):
         'location': "You are in: ", 'score': "Your score is: ", 'inventory': "your inventory opens!"})
 
     Tunnel = Zone("Tunnel", "it's dark and you can sense something folowing you",
-    "Soon you see a bright light and as you go towards it your area begins to change.",
-    "You feel the tunnel begin to move and soon you are enveloped by the floor", [None],
-    {"map": "You pull out the map and see that you must make a right turn", 
-    "left": "You take the left-most path, but its a dead end...", "right": "you take the right-most path",
-    'location': "You are in: ", 'score': "Your score is: ", 'inventory': "your inventory opens!"})
+        "Soon you see a bright light and as you go towards it your area begins to change.",
+        "You feel the tunnel begin to move and soon you are enveloped by the floor", [None],
+        {"map": "You pull out the map and see that you must make a right turn", 
+        "left": "You take the left-most path, but its a dead end...", "right": "you take the right-most path",
+        'location': "You are in: ", 'score': "Your score is: ", 'inventory': "your inventory opens!"})
 
     House = Zone("House", "Feels safe and cozy...", "Suddenly, something feels strange...",
         "text", [None], {"wander": "you begin to look in all of the rooms",
