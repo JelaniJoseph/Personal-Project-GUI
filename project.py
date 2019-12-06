7606fc9f5948c883584f44d48673aacf6605cff5
Jelani.Joseph <jelani.joseph1@marist.edu>
2019-12-06 00:01:28 -0500

    Try again working, now needing item situational

--- project.py
+++ project.py
@@ -10,17 +10,16 @@ from items import *
 def project():
     locations = locale_data(backpack,Wisp_in_bottle,coat,coin)
     player = Player_Data(locations)
-    title(locations)
-    player = Player_Data(locations)
+    conditional = True
+    title()
     playercustom(player)
-    game_loop(locations, player)
-    email_jel = str("jelani.joseph1@marist.edu")
-    end_screen(email_jel, player)
+    game_loop(locations, conditional, player)
+    end_screen(player, locations, conditional)
 
 
 # Displays the title screen and Intro
 # Allows player to choose username
-def title(locations):
+def title():
     print("Welcome to Bio-Saga")
     print("=========================\n")
     print("Backstory:\n")
@@ -41,30 +40,32 @@ def title(locations):
 def playercustom(player):
     player.setname()
 
-
-# Function to add to score, and show location visited
-def goto(player, score, locations):
-    score = player.update_score()
-    player.next_loc(locations)
-    return(score)
-
-
-# Function takes user input and returns input against the available commands
-# After 10 inputs the user will "run out of time" and will be exited out of the loop
-def get_int(user, actions, player, actions_taken):
-    player.timer(actions, actions_taken)
-
+def replay(player, locations, conditional):
+    print("The game is over, but would you like to replay?")
+    replay_choice = input('y/n')
+    if replay_choice =='y':
+        reset_locations = locale_data(backpack,Wisp_in_bottle,coat,coin)
+        reset_player = Player_Data(locations)
+        game_loop(reset_locations, True, reset_player)
+    elif replay_choice =='n':
+        print("Thanks for playing!")
+        quit()
+    else:
+        print("invalid choice")
+        replay(player, locations, conditional)
 
 # Main game loop, prints the situation, and based on input locations vary
 # Commands available for user are listed before they are asked for input.
-def game_loop(locations, player):
-    while True:
+def game_loop(locations, conditional, player):
+    while conditional == True:
         player.loc_get().describe()
         player.update_score()
         player.loc_get().choose_action(player)
         player.set_location()
         locations[1].describe()
-        locations[1].forest_path_actions(locations, player)
+        conditional = locations[1].forest_path_actions(locations, player)
+        if conditional == False:
+            replay(locations, player, True)
         player.set_location()
         player.update_score()
         locations[2].describe()
@@ -76,7 +77,9 @@ def game_loop(locations, player):
         player.set_location()
         player.update_score()
         locations[4].describe()
-        locations[4].tundra_path_actions(locations, player)
+        conditional = locations[4].tundra_path_actions(locations, player)
+        if conditional == False:
+            replay(locations, player, True)
         player.set_location()
         player.update_score()
         locations[5].describe()
@@ -88,7 +91,9 @@ def game_loop(locations, player):
         player.set_location()
         player.update_score()
         locations[7].describe()
-        locations[7].tunnel_action_option(locations, player)
+        conditional = locations[7].tunnel_action_option(locations, player)
+        if conditional == False:
+            replay(locations, player, True)
         player.set_location()
         player.update_score()
         locations[8].describe()
@@ -97,14 +102,16 @@ def game_loop(locations, player):
         player.update_score()
         locations[9].describe()
         locations[9].choose_action(player)
+        break
 
-project()
 
-
-def end_screen(email_jel, player):
+def end_screen(player, locations, conditional):
     print("But then you wonder what was that strange experience you went through and who was that shadowy figure?")
     print("You decide it would be best to think about it later as right now your far too relieved to be alive.")
-    player.timer()
-    print("Copyright: This game was created by Jelani, and Daniel\n")
-    print("If you have any questions feel free to email " + email_jel)
-    return(email_jel)
+    print(player.getname(), "Finished the game in:", player.timer(), "steps!")
+    print("Copyright: This game was created by Jelani\n")
+    print("If you have any questions feel free to email jelani.joseph1@marist.edu")
+    replay(player, locations, conditional)
+ 
+project()
+
